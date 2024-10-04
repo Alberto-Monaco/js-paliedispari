7435bb571d07e7ac11d3016dfe52bcd3b2e4bda5
Alberto-Monaco <alberto.monaco84@gmail.com>
2024-10-04 14:46:04 +0200

    begin palindroma exercise

---
+++ palindroma/script.js
@@ -0,0 +1,8 @@
+//*Palindroma
+// Chiedere all’utente di inserire una parola Creare una funzione per capire se la parola inserita è palindroma
+
+const word = 'maria'
+const word_divise = word.split('')
+console.log(word_divise)
+const word_reverse = word_divise.reverse()
+console.log(word_reverse)
