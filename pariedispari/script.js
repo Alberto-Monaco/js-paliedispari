bad2b4fa7fab7ab8078d53a35a78d66d41e470aa
Alberto-Monaco <alberto.monaco84@gmail.com>
2024-10-04 14:24:20 +0200

    complete pari e dispari

---
+++ pariedispari/script.js
@@ -0,0 +1,26 @@
+//*Pari e Dispari
+// L’utente sceglie pari o dispari e inserisce un numero da 1 a 5. Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione).
+// Sommiamo i due numeri Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione) Dichiariamo chi ha vinto.
+
+const user_number = Number(prompt('scegli un numero tra 1 e 5'))
+alert(`hai scelto il numero ${user_number}`)
+
+function pc_number() {
+	const number = Math.round(Math.random() * 4) + 1
+	return number
+}
+
+const number_computer = pc_number()
+alert(`il computer ha scelto il numero ${number_computer}`)
+function pair() {
+	let somma = user_number + number_computer
+	return somma % 2 === 0
+}
+
+const result = pair()
+
+if (result) {
+	alert('la somma dei due numeri è pari')
+} else {
+	alert('la somma dei due numeri è dispari')
+}
